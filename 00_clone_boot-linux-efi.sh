370bc4b08c3519002d568559cd1b5aa002bc1edc
reinzcity <reinz.developer@gmail.com>
2022-12-10 22:27:45 +0700

    under maintenance

---
+++ 00_clone_boot-linux-efi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $HOME
+
+git clone https://github.com/reinzcity/boot-linux-efi.git
+if [[ "$?" != 0 ]]; then
+  if [[ -d "boot-linux-efi" ]]; then
+    echo $(pwd)
+    cd boot-linux-efi
+    echo $(pwd)
+    git reset --hard HEAD
+    git pull
+    bash ./install_boot.sh
+  fi
+else
+  echo $(pwd)
+  cd boot-linux-efi
+  echo $(pwd)
+  bash ./install_boot.sh
+fi
