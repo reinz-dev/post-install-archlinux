af9676a779ee83d08740749474e836b927274f81
reinzcity <reinz.developer@gmail.com>
2022-12-10 23:07:24 +0700

    under maintenance

--- mkinitcpio-presets/make.sh
+++ mkinitcpio-presets/make.sh
@@ -8,10 +8,16 @@ else
     echo "USER IS ROOT"
 fi
 
+echo "proses backup"
+
 mv /etc/mkinitcpio.d /etc/mkinitcpio.d.bak
 
 mv /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
 
 cp --recursive --force --remove-destination --no-dereference --preserve=links . /etc/
 
+echo "backup success"
+
 mkinitcpio -p linux
+
+echo "all done okey"
