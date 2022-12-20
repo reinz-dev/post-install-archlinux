90a98d7aa1a09ee074347edcc4996eeea312a29f
reinzcity <reinz.developer@gmail.com>
2022-12-21 00:17:13 +0700

    edit installer

--- 03_installer-dotfiles.sh
+++ 03_installer-dotfiles.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 
+## update pacman
+echo "update system"
 sudo pacman -Syu && sudo pacman -Syyu
 sudo pacman -S --needed base-devel
+
+## install and update paru aur
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 sudo rm -rf $HOME/paru
-
 paru -Syu && paru -Sua
 
-echo "##-------| installing |-------##"
+## installing package paru
+echo "installing package paru"
+paru -S nerd-fonts-complete
+paru -S extension-manager
+paru -S gnome-shell-extension-blur-my-shell
+paru -S gnome-shell-extension-dash-to-dock
+paru -S gnome-shell-extension-compiz-alike-magic-lamp-effect
+paru -S gnome-shell-extension-gtk-title-bar
+paru -S gnome-shell-extension-no-overview-at-start-up
+paru -S ntfs-3g
+
+## installing terminal 
+echo "installing terminal"
 sudo pacman -S kitty fish neofetch ranger tmux exa
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 chsh -s /usr/bin/fish
-echo " ##-------| all done |-------##"
-echo " "
 
-echo "##-------| installing starhip-shell |-------##"
+## installing starship shell
+echo "installing startship"
 curl -sS https://starship.rs/install.sh | sh
-echo "##-------| starhip done |-------##"
-echo " "
 
-echo "##-------| clone dotfiles |-------##"
+## clonning my dotfiles
+echo "installing dotfiles"
 cd $HOME
 git clone http://github.com/reinzcity/dotfiles.git
 cd dotfiles
 yes | cp -rf . ~
-echo "##-------| dotfiles done |-------##"
-echo " "
 
-echo "##-------| installing homebrew linux |-------##"
+## installing homebrew-linux
+echo "installing homebrew-linux"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-echo "##-------| homebrew done |-------##"
-
 brew -v
-brew install neovim --HEAD
-
+brew update
+brew install gcc
 cd $HOME
 
-echo "installing extension paru"
-paru -S nerd-fonts-complete
-paru -S extension-manager
-pari -S ntfs-3g
-
+## installing neovim 
 echo "installing Neovim"
+brew install neovim --HEAD
 git clone http://github.com/reinzcity/nvim-2.0
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 mkdir $HOME/.config/nvim
@@ -52,7 +59,12 @@ cd nvim-2.0
 cp -rf . ~/.config/nvim/
 
 cd $HOME
+rm -rf $HOME/nvim-2.0
+
+cd $HOME
 rm -rf $HOME/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/lua/
 mkdir $HOME/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/lua/
 cd $HOME/dotfiles/.repo/lua
 cp -rf . $HOME/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/lua/
+
+
